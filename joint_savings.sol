c0a3bd837f98c6a419d947700d29bab57479a42a
Aaron Montano <aarchmon@Aarons-MacBook-Pro.local>
2021-05-23 21:59:51 -0700

    Finished code and added Execution Results.

--- joint_savings.sol
+++ joint_savings.sol
@@ -42,12 +42,12 @@ contract JointSavings {
         /*
         Define a `require` statement that checks if the `recipient` is equal to either `accountOne` or `accountTwo`. The `requiere` statement returns the text `"You don't own this account!"` if it does not.
         */
-        require(recipient == accountOne || recipient == accountTwo, "You don't own this account! ")
+        require(recipient == accountOne || recipient == accountTwo, "You don't own this account! ");
 
         /*
         Define a `require` statement that checks if the `balance` is sufficient to accomplish the withdraw operation. If there are insufficient funds, the text `Insufficient funds!` is returned.
         */
-        require(address(this).balance <= amount, "Insufficient funds! ");
+        require(address(this).balance >= amount, "Insufficient funds! ");
 
         /*
         Add and `if` statement to check if the `lastToWithdraw` is not equal to (`!=`) to `recipient` If `lastToWithdraw` is not equal, then set it to the current value of `recipient`.
@@ -57,13 +57,13 @@ contract JointSavings {
         }
 
         // Call the `transfer` function of the `recipient` and pass it the `amount` to transfer as an argument.
-        trans
+        recipient.transfer(amount);
 
         // Set  `lastWithdrawAmount` equal to `amount`
         lastWithdrawAmount = amount;
 
         // Call the `contractBalance` variable and set it equal to the balance of the contract by using `address(this).balance` to reflect the new balance of the contract.
-        // YOUR CODE HERE!
+        contractBalance = address(this).balance;
     }
 
     // Define a `public payable` function named `deposit`.
@@ -72,7 +72,7 @@ contract JointSavings {
         /*
         Call the `contractBalance` variable and set it equal to the balance of the contract by using `address(this).balance`.
         */
-        // YOUR CODE HERE!
+        contractBalance = address(this).balance;
     }
 
     /*
@@ -81,11 +81,12 @@ contract JointSavings {
     function setAccounts(address payable account1, address payable account2) public{
 
         // Set the values of `accountOne` and `accountTwo` to `account1` and `account2` respectively.
-        // YOUR CODE HERE!
+        accountOne = account1;
+        accountTwo = account2;
     }
 
     /*
     Finally, add the **default fallback function** so that your contract can store Ether sent from outside the deposit function.
     */
-    // YOUR CODE HERE!
+    function() external payable {}
 }
